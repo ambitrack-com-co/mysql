0453ceb5b99675cec27d14168cd7b6e6a434d4ae
ambitrack-com-co <ambitrack.com.co@hotmail.com>
2025-10-23 11:54:48 -0500

    create table user

--- virtual-store.sql
+++ virtual-store.sql
@@ -1,2 +1,9 @@
-CREATE DATABASE virtual_store;
-USE virtual_store
+CREATE DATABASE IF NOT EXISTS virtual_store;
+USE virtual_store;
+CREATE TABLE IF NOT EXISTS user(
+Cedula_Ciudadania INT PRIMARY KEY,
+First_Name VARCHAR(50) NOT NULL,
+Second_Name VARCHAR(50) NULL,
+First_Last_Name VARCHAR(50) NOT NULL,
+Second_Last_Name VARCHAR(50) NULL 
+);
